5c43cd18861eb7ea9e60782b0ce49e31a2483c26
D. Mukesh Venkat Sai <dmukeshmanoj@gmail.com>
2026-02-27 14:44:05 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,26 @@
-import express from"express";
 import dotenv from"dotenv";
-import connectDB from"./config/db.js";
-
 dotenv.config();
-connectDB();
 
+import cors from"cors";
+import express from"express";
+import connectDB from"./config/db.js";
 import authRoutes from"./routes/authRoutes.js";
 import taskRoutes from"./routes/taskRoutes.js";
 
+connectDB();
 
 const app=express();
 
+app.use(cors({origin:"http://localhost:5173"}));
 app.use(express.json());
 
 app.use("/api/auth",authRoutes);
-app.use("/api/tasks",taskRoutes);
+app.use("/api/tasks",taskRoutes);4
 
 app.get("/",(req,res)=>{
-  res.send("Backend running");
+  res.send("Backend is connected");
 });
 
 app.listen(5000,()=>{
-  console.log("Server running on 5000");
+  console.log("Server is running on port 5000");
 });
