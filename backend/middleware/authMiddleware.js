50cf9964a57ea5c540046600afccf159a2e1c9f8
D. Mukesh Venkat Sai <dmukeshmanoj@gmail.com>
2026-02-27 13:37:38 +0530

    added JWT auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from"jsonwebtoken";
+
+export const protect=(req,res,next)=>{
+  const header=req.headers.authorization;
+  if(!header){
+    return res.status(401).json({
+        msg:"Not authorized"
+    });
+  }
+
+  const token=header.split(" ")[1];
+
+  try{
+    const decoded=jwt.verify(token,process.env.JWT_SECRET);
+    req.user={id:decoded.id};
+    next();
+  }catch(err){
+    res.status(401).json({
+        msg:"Invalid token"
+    });
+  }
+};
